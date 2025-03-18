fbbec33c4dc36eb9075231d898aa80d97268b796
sahajpatel008 <sahajpatel008@gmail.com>
2025-03-18 00:38:30 -0700

    gifs -> image

--- src/main/java/com/example/gridproject/PlantGridController.java
+++ src/main/java/com/example/gridproject/PlantGridController.java
@@ -15,7 +15,6 @@ import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.application.Platform;
-import javafx.scene.text.Text;
 
 import java.io.IOException;
 import java.util.*;
@@ -37,7 +36,6 @@ import javafx.util.Duration;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import org.controlsfx.control.tableview2.filter.filtereditor.SouthFilter;
 
 class InsectViewInfo {
     ImageView insectView;
@@ -129,10 +127,10 @@ public class PlantGridController {
     private ScheduledExecutorService scheduler;
 
     public void initialize() {
-        gifSunny = new Image(Objects.requireNonNull(getClass().getResource("/images/sunCool.gif")).toExternalForm(), true);
-        gifRainy = new Image(Objects.requireNonNull(getClass().getResource("/images/rain1.gif")).toExternalForm(), true);
-        gifCloudy = new Image(Objects.requireNonNull(getClass().getResource("/images/cloudyWO_BG.gif")).toExternalForm(), true);
-        gifClear = new Image(Objects.requireNonNull(getClass().getResource("/images/clear1.gif")).toExternalForm(), true);
+        gifSunny = new Image(Objects.requireNonNull(getClass().getResource("/images/sunnyImage.png")).toExternalForm(), true);
+        gifRainy = new Image(Objects.requireNonNull(getClass().getResource("/images/rainImage.png")).toExternalForm(), true);
+        gifCloudy = new Image(Objects.requireNonNull(getClass().getResource("/images/cloudyImage.png")).toExternalForm(), true);
+        gifClear = new Image(Objects.requireNonNull(getClass().getResource("/images/clearImage.png")).toExternalForm(), true);
 
         // Set initial weather image
         if (currentWeatherGif != null) {
